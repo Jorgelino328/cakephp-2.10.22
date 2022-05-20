3fcf9e937c85bce7666e13fe958dcf640c4eaab5
Jorgelino328 <jorgewilliam328@gmail.com>
2022-05-20 16:49:57 -0300

    Blog almost ready

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -2,46 +2,73 @@
 
 App::uses('AppController', 'Controller');
 
-class PostsController extends AppController {
-    public $helpers = array ('Html','Form');
-    public $name = 'Posts';
+class PostsController extends AppController
+{
+	public $helpers = array('Html', 'Form');
+	public $name = 'Posts';
 
-    function index() {
-        $this->set('posts', $this->Post->find('all'));
-    }
+	function index()
+	{
+		$this->set('posts', $this->Post->find('all'));
+	}
 
-    public function view($id = null) {
-        $this->set('post', $this->Post->findById($id));
-    }
+	public function view($id = null)
+	{
+		$this->set('post', $this->Post->findById($id));
+	}
 
-    public function add() {
-        if ($this->request->is('post')) {
-            if ($this->Post->save($this->request->data)) {
-                $this->Flash->success('Your post has been saved.');
-                $this->redirect(array('action' => 'index'));
-            }
-        }
-    }
+	public function isAuthorized($user)
+	{
+		// All registered users can add posts
+		if ($this->action === 'add') {
+			return true;
+		}
 
-    function edit($id = null) {
-        $this->Post->id = $id;
-        if ($this->request->is('get')) {
-            $this->request->data = $this->Post->findById($id);
-        } else {
-            if ($this->Post->save($this->request->data)) {
-                $this->Flash->success('Your post has been updated.');
-                $this->redirect(array('action' => 'index'));
-            }
-        }
-    }
+		// The owner of a post can edit and delete it
+		if (in_array($this->action, array('edit', 'delete'))) {
+			$postId = (int)$this->request->params['pass'][0];
+			if ($this->Post->isOwnedBy($postId, $user['id'])) {
+				return true;
+			}
+		}
 
-    function delete($id) {
-        if (!$this->request->is('post')) {
-            throw new MethodNotAllowedException();
-        }
-        if ($this->Post->delete($id)) {
-            $this->Flash->success('The post with id: ' . $id . ' has been deleted.');
-            $this->redirect(array('action' => 'index'));
-        }
-    }
+		return parent::isAuthorized($user);
+	}
+
+	public function add()
+	{
+		if ($this->request->is('post')) {
+			$this->request->data['Post']['user_id'] = $this->Auth->user('id');
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__('Your post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+		}
+	}
+
+	function edit($id = null)
+	{
+		$this->Post->id = $id;
+		if ($this->request->is('get')) {
+			$this->request->data = $this->Post->findById($id);
+		} else {
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success('Your post has been updated.');
+				$this->redirect(array('action' => 'index'));
+			}
+		}
+	}
+
+	function delete($id)
+	{
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		if ($this->Post->delete($id)) {
+			$this->Flash->success('The post with id: ' . $id . ' has been deleted.');
+			$this->redirect(array('action' => 'index'));
+		}
+	}
+
+
 }
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,16 +1,22 @@
 <?php
 
-App::uses('Model', 'Model');
+App::uses('AppModel', 'Model');
 
-class Post extends AppModel {
-    public $name = 'Post';
+class Post extends AppModel
+{
+	public $name = 'Post';
 
-    public $validate = array(
-        'title' => array(
-            'rule' => 'notBlank'
-        ),
-        'body' => array(
-            'rule' => 'notBlank'
-        )
-    );
+	public $validate = array(
+		'title' => array(
+			'rule' => 'notBlank'
+		),
+		'body' => array(
+			'rule' => 'notBlank'
+		)
+	);
+
+	public function isOwnedBy($post, $user)
+	{
+		return $this->field('id', array('id' => $post, 'user_id' => $user)) === $post;
+	}
 }
